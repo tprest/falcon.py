91dcfb2f4e560b736d194ced67264b914a2b9833
Thomas Prest <thomas.prest@pqshield.com>
2026-01-13 15:45:05 +0100

    cleaner API: allows to export keys as bytestrings

--- falcon.py
+++ falcon.py
@@ -10,15 +10,14 @@ from ntt import sub_zq, mul_zq, div_zq
 from ffsampling import gram, ffldl_fft, ffsampling_fft
 from ntrugen import ntru_gen
 from encoding import compress, decompress
+from dataclasses import dataclass
 # https://pycryptodome.readthedocs.io/en/latest/src/hash/shake256.html
 from Crypto.Hash import SHAKE256
 # Randomness
 from os import urandom
 from rng import ChaCha20
 # For debugging purposes
-import sys
-if sys.version_info >= (3, 4):
-    from importlib import reload  # Python 3.4+ only.
+from beartype import beartype
 
 
 set_printoptions(linewidth=200, precision=5, suppress=True)
@@ -43,93 +42,104 @@ SALT_LEN = 40
 SEED_LEN = 56
 
 
-# Parameter sets for Falcon:
-# - n is the dimension/degree of the cyclotomic ring
-# - sigma is the std. dev. of signatures (Gaussians over a lattice)
-# - sigmin is a lower bounds on the std. dev. of each Gaussian over Z
-# - sigbound is the upper bound on ||s0||^2 + ||s1||^2
-# - sig_bytelen is the bytelength of signatures
-Params = {
-    # FalconParam(2, 2)
-    2: {
-        "n": 2,
-        "sigma": 144.81253976308423,
-        "sigmin": 1.1165085072329104,
-        "sig_bound": 101498,
-        "sig_bytelen": 44,
-    },
-    # FalconParam(4, 2)
-    4: {
-        "n": 4,
-        "sigma": 146.83798833523608,
-        "sigmin": 1.1321247692325274,
-        "sig_bound": 208714,
-        "sig_bytelen": 47,
-    },
-    # FalconParam(8, 2)
-    8: {
-        "n": 8,
-        "sigma": 148.83587593064718,
-        "sigmin": 1.147528535373367,
-        "sig_bound": 428865,
-        "sig_bytelen": 52,
-    },
-    # FalconParam(16, 4)
-    16: {
-        "n": 16,
-        "sigma": 151.78340713845503,
-        "sigmin": 1.170254078853483,
-        "sig_bound": 892039,
-        "sig_bytelen": 63,
-    },
-    # FalconParam(32, 8)
-    32: {
-        "n": 32,
-        "sigma": 154.6747794602761,
-        "sigmin": 1.1925466358390344,
-        "sig_bound": 1852696,
-        "sig_bytelen": 82,
-    },
-    # FalconParam(64, 16)
-    64: {
-        "n": 64,
-        "sigma": 157.51308555044122,
-        "sigmin": 1.2144300507766141,
-        "sig_bound": 3842630,
-        "sig_bytelen": 122,
-    },
-    # FalconParam(128, 32)
-    128: {
-        "n": 128,
-        "sigma": 160.30114421975344,
-        "sigmin": 1.235926056771981,
-        "sig_bound": 7959734,
-        "sig_bytelen": 200,
-    },
-    # FalconParam(256, 64)
-    256: {
-        "n": 256,
-        "sigma": 163.04153322607107,
-        "sigmin": 1.2570545284063217,
-        "sig_bound": 16468416,
-        "sig_bytelen": 356,
-    },
-    # FalconParam(512, 128)
-    512: {
-        "n": 512,
-        "sigma": 165.7366171829776,
-        "sigmin": 1.2778336969128337,
-        "sig_bound": 34034726,
-        "sig_bytelen": 666,
-    },
-    # FalconParam(1024, 256)
-    1024: {
-        "n": 1024,
-        "sigma": 168.38857144654395,
-        "sigmin": 1.298280334344292,
-        "sig_bound": 70265242,
-        "sig_bytelen": 1280,
-    },
+@dataclass
+class FalconParam:
+    """
+    Dataclass for Falcon parameters.
+    - n is the dimension/degree of the cyclotomic ring
+    - sigma is the std. dev. of signatures (Gaussians over a lattice)
+    - sigmin is a lower bounds on the std. dev. of each Gaussian over Z
+    - sigbound is the upper bound on ||s0||^2 + ||s1||^2
+    - sig_bytelen is the bytelength of signatures
+    """
+    n: int
+    sigma: float
+    sigmin: float
+    sig_bound: int
+    sig_bytelen: int
+
+
+params = {
+    # FalconParam(2)
+    2: FalconParam(
+        n=2,
+        sigma=144.81253976308423,
+        sigmin=1.1165085072329104,
+        sig_bound=101498,
+        sig_bytelen=44,
+    ),
+    # FalconParam(4)
+    4: FalconParam(
+        n=4,
+        sigma=146.83798833523608,
+        sigmin=1.1321247692325274,
+        sig_bound=208714,
+        sig_bytelen=47,
+    ),
+    # FalconParam(8)
+    8: FalconParam(
+        n=8,
+        sigma=148.83587593064718,
+        sigmin=1.147528535373367,
+        sig_bound=428865,
+        sig_bytelen=52,
+    ),
+    # FalconParam(16)
+    16: FalconParam(
+        n=16,
+        sigma=151.78340713845503,
+        sigmin=1.170254078853483,
+        sig_bound=892039,
+        sig_bytelen=63,
+    ),
+    # FalconParam(32)
+    32: FalconParam(
+        n=32,
+        sigma=154.6747794602761,
+        sigmin=1.1925466358390344,
+        sig_bound=1852696,
+        sig_bytelen=82,
+    ),
+    # FalconParam(64)
+    64: FalconParam(
+        n=64,
+        sigma=157.51308555044122,
+        sigmin=1.2144300507766141,
+        sig_bound=3842630,
+        sig_bytelen=122,
+    ),
+    # FalconParam(128)
+    128: FalconParam(
+        n=128,
+        sigma=160.30114421975344,
+        sigmin=1.235926056771981,
+        sig_bound=7959734,
+        sig_bytelen=200,
+    ),
+    # FalconParam(256)
+    256: FalconParam(
+        n=256,
+        sigma=163.04153322607107,
+        sigmin=1.2570545284063217,
+        sig_bound=16468416,
+        sig_bytelen=356,
+    ),
+    # FalconParam(512)
+    512: FalconParam(
+        n=512,
+        sigma=165.7366171829776,
+        sigmin=1.2778336969128337,
+        sig_bound=34034726,
+        sig_bytelen=666,
+    ),
+    # FalconParam(1024)
+    1024: FalconParam(
+        n=1024,
+        sigma=168.38857144654395,
+        sigmin=1.298280334344292,
+        sig_bound=70265242,
+        sig_bytelen=1280,
+    )
 }
 
 
@@ -180,95 +190,71 @@ def normalize_tree(tree, sigma):
         tree[1] = 0
 
 
-class PublicKey:
+@beartype
+def serialize_poly(poly: list[int]) -> bytes:
     """
-    This class contains methods for performing public key operations in Falcon.
+    Serialize a polynomial (a list of integers) into a bytestring.
+    We assume that all entries are between 0 and q - 1.
     """
+    n = len(poly)
+    if (min(poly) < 0) or (max(poly) >= q):
+        raise ValueError("The entries of poly are outside bounds")
 
-    def __init__(self, sk):
-        """Initialize a public key."""
-        self.n = sk.n
-        self.h = sk.h
-        self.hash_to_point = sk.hash_to_point
-        self.signature_bound = sk.signature_bound
-        self.verify = sk.verify
-
-    def __repr__(self):
-        """Print the object in readable form."""
-        rep = "Public for n = {n}:\n\n".format(n=self.n)
-        rep += "h = {h}\n".format(h=self.h)
-        return rep
+    BITS_PER_COEF = 14
+    int_buffer = 0
+    for idx in range(n):
+        int_buffer ^= poly[idx] << (idx * BITS_PER_COEF)
+    # The "+ 7" allows to round to the nearest higher integer.
+    bytelen = (n * BITS_PER_COEF + 7) >> 3
+    return int_buffer.to_bytes(bytelen)
 
 
-class SecretKey:
+@beartype
+def deserialize_to_poly(bytestring: bytes, n: int) -> list[int]:
     """
-    This class contains methods for performing
-    secret key operations (and also public key operations) in Falcon.
-
-    One can:
-    - initialize a secret key for:
-        - n = 128, 256, 512, 1024,
-        - phi = x ** n + 1,
-        - q = 12 * 1024 + 1
-    - find a preimage t of a point c (both in ( Z[x] mod (Phi,q) )**2 ) such that t*B0 = c
-    - hash a message to a point of Z[x] mod (Phi,q)
-    - sign a message
-    - verify the signature of a message
+    Deserialize a bytestring to a polynomial (a list of integers).
+    We assume that all entries are between 0 and q - 1.
     """
+    assert (8 * len(bytestring) % n == 0)
+    BITS_PER_COEF = 14
+    mask = (1 << BITS_PER_COEF) - 1
 
-    def __init__(self, n, polys=None):
-        """Initialize a secret key."""
-        # Public parameters
-        self.n = n
-        self.sigma = Params[n]["sigma"]
-        self.sigmin = Params[n]["sigmin"]
-        self.signature_bound = Params[n]["sig_bound"]
-        self.sig_bytelen = Params[n]["sig_bytelen"]
+    poly = [0] * n
+    int_buffer = int.from_bytes(bytestring)
+    for idx in range(n):
+        poly[idx] = int_buffer & mask
+        int_buffer >>= BITS_PER_COEF
+    return poly
 
-        # Compute NTRU polynomials f, g, F, G verifying fG - gF = q mod Phi
-        if polys is None:
-            self.f, self.g, self.F, self.G = ntru_gen(n)
+
+@dataclass
+class Falcon:
+    """
+    A dataclass for Falcon.
+
+    Typical example of how to use this class:
+
+    >>> falcon = Falcon(n).  # n a power-of-two between 2 and 1024
+    >>> sk, vk = falcon.keygen()
+    >>> sig = falcon.sign(sk, msg)
+    >>> assert (falcon.verify(vk, msg, sig) == True)
+
+    """
+    param: FalconParam
+
+    @beartype
+    def __init__(self, n: int):
+        if n in params:
+            self.param = params[n]
         else:
-            [f, g, F, G] = polys
-            assert all((len(poly) == n) for poly in [f, g, F, G])
-            self.f = f[:]
-            self.g = g[:]
-            self.F = F[:]
-            self.G = G[:]
+            raise ValueError
 
-        # From f, g, F, G, compute the basis B0 of a NTRU lattice
-        # as well as its Gram matrix and their fft's.
-        B0 = [[self.g, neg(self.f)], [self.G, neg(self.F)]]
-        G0 = gram(B0)
-        self.B0_fft = [[fft(elt) for elt in row] for row in B0]
-        G0_fft = [[fft(elt) for elt in row] for row in G0]
-
-        self.T_fft = ffldl_fft(G0_fft)
-
-        # Normalize Falcon tree
-        normalize_tree(self.T_fft, self.sigma)
-
-        # The public key is a polynomial such that h*f = g mod (Phi,q)
-        self.h = div_zq(self.g, self.f)
-
-    def __repr__(self, verbose=False):
-        """Print the object in readable form."""
-        rep = "Private key for n = {n}:\n\n".format(n=self.n)
-        rep += "f = {f}\n".format(f=self.f)
-        rep += "g = {g}\n".format(g=self.g)
-        rep += "F = {F}\n".format(F=self.F)
-        rep += "G = {G}\n".format(G=self.G)
-        if verbose:
-            rep += "\nFFT tree\n"
-            rep += print_tree(self.T_fft, pref="")
-        return rep
-
-    def hash_to_point(self, message, salt):
+    def __hash_to_point__(self, message: bytes, salt: bytes) -> list[int]:
         """
         Hash a message to a point in Z[x] mod(Phi, q).
         Inspired by the Parse function from NewHope.
         """
-        n = self.n
+        n = self.param.n
         if q > (1 << 16):
             raise ValueError("The modulus is too large")
 
@@ -290,19 +276,59 @@ class SecretKey:
                 i += 1
         return hashed
 
-    def sample_preimage(self, point, seed=None):
+    @beartype
+    def pack_sk(self, sk) -> bytes:
+        """
+        Pack a Falcon secret key into a bytestring.
+        To be used in conjunction with unpack_sk.
+        
+        Not used in this module but can be useful for exporting keys.
+        """
+        (f, g, F, G, B0_fft, T_fft) = sk
+        sk_bytes = b""
+        for poly in (f, g, F, G):
+            # We reduce modulo q before serializing
+            sk_bytes += serialize_poly([coef % q for coef in poly])
+        return sk_bytes
+
+    @beartype
+    def unpack_sk(self, sk_bytes: bytes):
+        """
+        Unpack a bytestring to a Falcon secret key.
+        
+        How to use:
+
+        >>> falcon = Falcon(n)
+        >>> sk, _ = falcon.keygen()
+        >>> sk_bytes = falcon.pack(sk)
+        >>> sk_2 = falcon.unpack_sk(sk_bytes)
+        >>> assert (sk == sk2)
+        
+        """
+        assert (len(sk_bytes) % 4 == 0)
+        # There are four polys in sk
+        len_poly = len(sk_bytes) // 4
+
+        polys = [None, None, None, None]
+        for i in range(4):
+            polys[i] = deserialize_to_poly(sk_bytes[i * len_poly:(i + 1) * len_poly], self.param.n)
+            polys[i] = [((coef + (q >> 1)) % q) - (q >> 1) for coef in polys[i]]
+        sk, _ = self.keygen(polys)
+        return sk
+
+    def __sample_preimage__(self, B0_fft, T_fft, point, seed=None):
         """
         Sample a short vector s such that s[0] + s[1] * h = point.
         """
-        [[a, b], [c, d]] = self.B0_fft
+        [[a, b], [c, d]] = B0_fft
 
         # We compute a vector t_fft such that:
         #     (fft(point), fft(0)) * B0_fft = t_fft
         # Because fft(0) = 0 and the inverse of B has a very specific form,
         # we can do several optimizations.
         point_fft = fft(point)
-        t0_fft = [(point_fft[i] * d[i]) / q for i in range(self.n)]
-        t1_fft = [(-point_fft[i] * b[i]) / q for i in range(self.n)]
+        t0_fft = [(point_fft[i] * d[i]) / q for i in range(self.param.n)]
+        t1_fft = [(-point_fft[i] * b[i]) / q for i in range(self.param.n)]
         t_fft = [t0_fft, t1_fft]
 
         # We now compute v such that:
@@ -310,12 +336,12 @@ class SecretKey:
         #     v is close to (point, 0)
         if seed is None:
             # If no seed is defined, use urandom as the pseudo-random source.
-            z_fft = ffsampling_fft(t_fft, self.T_fft, self.sigmin, urandom)
+            z_fft = ffsampling_fft(t_fft, T_fft, self.param.sigmin, urandom)
         else:
             # If a seed is defined, initialize a ChaCha20 PRG
             # that is used to generate pseudo-randomness.
             chacha_prng = ChaCha20(seed)
-            z_fft = ffsampling_fft(t_fft, self.T_fft, self.sigmin,
+            z_fft = ffsampling_fft(t_fft, T_fft, self.param.sigmin,
                                    chacha_prng.randombytes)
 
         v0_fft = add_fft(mul_fft(z_fft[0], a), mul_fft(z_fft[1], c))
@@ -329,43 +355,84 @@ class SecretKey:
         s = [sub(point, v0), neg(v1)]
         return s
 
-    def sign(self, message, randombytes=urandom):
+    @beartype
+    def keygen(self, polys: None|list[list[int]]=None):
+        """
+        Initialize a secret key.
+        """
+        # Public parameters
+        n = self.param.n
+
+        # Compute NTRU polynomials f, g, F, G verifying fG - gF = q mod Phi
+        if polys is None:
+            f, g, F, G = ntru_gen(n)
+        else:
+            assert all((len(poly) == n) for poly in polys)
+            [f, g, F, G] = [poly[:] for poly in polys]
+
+        # From f, g, F, G, compute the basis B0 of a NTRU lattice
+        # as well as its Gram matrix and their fft's.
+        B0 = [[g, neg(f)], [G, neg(F)]]
+        G0 = gram(B0)
+        B0_fft = [[fft(elt) for elt in row] for row in B0]
+        G0_fft = [[fft(elt) for elt in row] for row in G0]
+
+        T_fft = ffldl_fft(G0_fft)
+
+        # Normalize Falcon tree
+        normalize_tree(T_fft, self.param.sigma)
+
+        # The public key is a polynomial such that h*f = g mod (Phi,q)
+        h = div_zq(g, f)
+
+        sk = (f, g, F, G, B0_fft, T_fft)
+        vk = serialize_poly(h)
+        return (sk, vk)
+
+    @beartype
+    def sign(self, sk, message: bytes, randombytes=urandom) -> bytes:
         """
         Sign a message. The message MUST be a byte string or byte array.
         Optionally, one can select the source of (pseudo-)randomness used
         (default: urandom).
         """
-        int_header = 0x30 + logn[self.n]
+        (f, g, F, G, B0_fft, T_fft) = sk
+
+        int_header = 0x30 + logn[self.param.n]
         header = int_header.to_bytes(1, "little")
 
         salt = randombytes(SALT_LEN)
-        hashed = self.hash_to_point(message, salt)
+        hashed = self.__hash_to_point__(message, salt)
 
         # We repeat the signing procedure until we find a signature that is
         # short enough (both the Euclidean norm and the bytelength)
         while (1):
             if (randombytes == urandom):
-                s = self.sample_preimage(hashed)
+                s = self.__sample_preimage__(B0_fft, T_fft, hashed)
             else:
                 seed = randombytes(SEED_LEN)
-                s = self.sample_preimage(hashed, seed=seed)
+                s = self.__sample_preimage__(B0_fft, T_fft, hashed, seed=seed)
             norm_sign = sum(coef ** 2 for coef in s[0])
             norm_sign += sum(coef ** 2 for coef in s[1])
             # Check the Euclidean norm
-            if norm_sign <= self.signature_bound:
-                enc_s = compress(s[1], self.sig_bytelen - HEAD_LEN - SALT_LEN)
+            if norm_sign <= self.param.sig_bound:
+                enc_s = compress(s[1], self.param.sig_bytelen - HEAD_LEN - SALT_LEN)
                 # Check that the encoding is valid (sometimes it fails)
                 if (enc_s is not False):
                     return header + salt + enc_s
 
-    def verify(self, message, signature):
+    @beartype
+    def verify(self, vk: bytes, message: bytes, signature: bytes) -> bool:
         """
         Verify a signature.
         """
-        # Unpack the salt and the short polynomial s1
+        # Unpack vk, the salt and the short polynomial s1
+        if (8 * len(vk) % self.param.n != 0):
+            raise ValueError(f"This does not seem to be a valid key for Falcon-{self.param.n}")
+        h = deserialize_to_poly(vk, self.param.n)
         salt = signature[HEAD_LEN:HEAD_LEN + SALT_LEN]
         enc_s = signature[HEAD_LEN + SALT_LEN:]
-        s1 = decompress(enc_s, self.sig_bytelen - HEAD_LEN - SALT_LEN, self.n)
+        s1 = decompress(enc_s, self.param.sig_bytelen - HEAD_LEN - SALT_LEN, self.param.n)
 
         # Check that the encoding is valid
         if (s1 is False):
@@ -373,14 +440,14 @@ class SecretKey:
             return False
 
         # Compute s0 and normalize its coefficients in (-q/2, q/2]
-        hashed = self.hash_to_point(message, salt)
-        s0 = sub_zq(hashed, mul_zq(s1, self.h))
+        hashed = self.__hash_to_point__(message, salt)
+        s0 = sub_zq(hashed, mul_zq(s1, h))
         s0 = [(coef + (q >> 1)) % q - (q >> 1) for coef in s0]
 
         # Check that the (s0, s1) is short
         norm_sign = sum(coef ** 2 for coef in s0)
         norm_sign += sum(coef ** 2 for coef in s1)
-        if norm_sign > self.signature_bound:
+        if norm_sign > self.param.sig_bound:
             print("Squared norm of signature is too large:", norm_sign)
             return False
 
--- test.py
+++ test.py
@@ -13,8 +13,8 @@ from ffsampling import gram
 from random import randint, random, gauss, uniform
 from math import sqrt, ceil
 from ntrugen import karamul, ntru_gen, gs_norm
-from falcon import SecretKey, PublicKey, Params
-from falcon import SALT_LEN, HEAD_LEN, SHAKE256
+from falcon import Falcon, params
+from falcon import SHAKE256, HEAD_LEN, SALT_LEN
 from encoding import compress, decompress
 from scripts import saga
 from scripts.samplerz_KAT512 import sampler_KAT512
@@ -141,7 +141,8 @@ def test_compress(n, iterations):
     """Test compression and decompression."""
     try:
         sigma = 1.5 * sqrt(q)
-        slen = Params[n]["sig_bytelen"] - SALT_LEN - HEAD_LEN
+        param = params[n]
+        slen = param.sig_bytelen - SALT_LEN - HEAD_LEN
     except KeyError:
         return True
     for i in range(iterations):
@@ -224,17 +225,17 @@ def test_signature(n, iterations=10):
     g = sign_KAT[n][0]["g"]
     F = sign_KAT[n][0]["F"]
     G = sign_KAT[n][0]["G"]
-    sk = SecretKey(n, [f, g, F, G])
-    pk = PublicKey(sk)
+    falcon = Falcon(n)
+    sk, vk = falcon.keygen([f, g, F, G])
     for i in range(iterations):
         message = b"abc"
-        sig = sk.sign(message)
-        if pk.verify(message, sig) is False:
+        sig = falcon.sign(sk, message)
+        if falcon.verify(vk, message, sig) is False:
             return False
     return True
 
 
-def test_sign_KAT():
+def test_unpack_and_sign_KAT():
     """
     Test the signing procedure against test vectors obtained from
     the Round 3 implementation of Falcon.
@@ -252,11 +253,20 @@ def test_sign_KAT():
             g = D["g"]
             F = D["F"]
             G = D["G"]
-            sk = SecretKey(n, [f, g, F, G])
+            falcon = Falcon(n)
+            sk, vk = falcon.keygen([f, g, F, G])
+
+            # We also test if packing/unpacking sk is correct: pack and unpack sk,
+            # and check that the unpacked sk2 is identical to sk.
+            sk_bytes = falcon.pack_sk(sk)
+            sk2 = falcon.unpack_sk(sk_bytes)
+            if (sk != sk2):
+                return False
+
             # The next line is done to synchronize the SHAKE256 context
             # with the one in the Round 3 C implementation of Falcon.
             _ = shake.read(8 * D["read_bytes"])
-            sig = sk.sign(message, shake.read)
+            sig = falcon.sign(sk, message, shake.read)
             if sig != bytes.fromhex(D["sig"]):
                 return False
     return True
@@ -294,7 +304,7 @@ def test(n, iterations=500):
     wrapper_test(test_ffnp, "ffNP", n, iterations)
     # test_compress and test_signature are only performed
     # for parameter sets that are defined.
-    if (n in Params):
+    if (n in params):
         wrapper_test(test_compress, "Compress", n, iterations)
         wrapper_test(test_signature, "Signature", n, iterations)
         # wrapper_test(test_sign_KAT, "Signature KATs", n, iterations)
@@ -304,13 +314,13 @@ def test(n, iterations=500):
 # Run all the tests
 if (__name__ == "__main__"):
     print("Test Sig KATs       : ", end="")
-    print("OK" if (test_sign_KAT() is True) else "Not OK")
+    print("OK" if (test_unpack_and_sign_KAT() is True) else "Not OK")
 
     # wrapper_test(test_samplerz_simple, "SamplerZ", None, 100000)
     wrapper_test(test_samplerz_KAT, "SamplerZ KATs", None, 1)
     print("")
 
-    for i in range(6, 11):
+    for i in range(6, 9):
         n = (1 << i)
         it = 1000
         print("Test battery for n = {n}".format(n=n))
